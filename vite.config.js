5e382305771d4290639cfbaf6a95400f4173b86e
Shabeebkaruvath <justshabeeb@gmail.com>
2025-05-03 16:05:48 +0530

    setup environment variable

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Shabeebkaruvath/Basket',
   plugins: [
     react(),
     tailwindcss(),
